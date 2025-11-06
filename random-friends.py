d4ef3d2a0096303ee6f5852d1572369b8a00f915
Vick Bouloute <vick@htmlbeyond.com>
2025-11-06 15:09:42 -0600

    initial files

---
+++ random-friends.py
@@ -0,0 +1,9 @@
+import random
+
+print("Who's going to pay the bill? ")
+friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+payer = random.choice(friends)
+
+print(f"{payer} is going to pay the bill.")
+
